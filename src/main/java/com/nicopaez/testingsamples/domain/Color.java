94defb53abd26c94b7f8986968acbe1baf938de6
NicoPaez <nicolas.paez@grupoesfera.com.ar>
2021-06-16 21:14:13 -0300

    Solution reseted

--- src/main/java/com/nicopaez/testingsamples/domain/Color.java
+++
@@ -1,9 +0,0 @@
-package com.nicopaez.testingsamples.domain;
-
-public enum Color {
-
-    ROJO,
-    AMARILLO,
-    AZUL
-}
-
--- src/test/java/com/nicopaez/testingsamples/domain/SemaforoDeTemperaturaTest.java
+++ src/test/java/com/nicopaez/testingsamples/domain/SemaforoDeTemperaturaTest.java
@@ -6,21 +6,4 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SemaforoDeTemperaturaTest {
 
-    @Test
-    public void EsRojoCuandoTemperaturaMayorIgualA30(){
-        SemaforoDeTemperatura semaforo = new SemaforoDeTemperatura();
-        assertThat(semaforo.getColorPara(30)).isEqualTo(Color.ROJO);
-    }
-
-    @Test
-    public void EsAmarilloCuandoTemperaturaEntre15y30(){
-        SemaforoDeTemperatura semaforo = new SemaforoDeTemperatura();
-        assertThat(semaforo.getColorPara(15)).isEqualTo(Color.AMARILLO);
-    }
-
-    @Test
-    public void EsAzuloCuandoTemperaturaMenorA15(){
-        SemaforoDeTemperatura semaforo = new SemaforoDeTemperatura();
-        assertThat(semaforo.getColorPara(10)).isEqualTo(Color.AZUL);
-    }
 }
